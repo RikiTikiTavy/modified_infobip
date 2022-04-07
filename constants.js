489af5b577cda945a42bd76cf0ffd661d1224a97
Созонов Роман <roman.sozonov@glowbyteconsulting.com>
2022-04-07 21:01:09 +0300

    first commit

---
+++ constants.js
@@ -0,0 +1,9 @@
+define('constants', [], function () {
+
+    return {
+        availableSenders: [],
+        sms: {
+            testUrl: "https://api.infobip.com/sms/1/text/single"
+        }
+    }
+});
