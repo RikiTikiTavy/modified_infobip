acfa34380d3eeeebca9736c9359f66cc124bca7a
Созонов Роман <roman.sozonov@glowbyteconsulting.com>
2022-04-13 23:13:57 +0300

    test url removed

--- constants.js
+++ constants.js
@@ -3,7 +3,7 @@ define('constants', [], function () {
     return {
         availableSenders: [],
         sms: {
-            testUrl: "https://eogllnkn7vg33qs.m.pipedream.net"
+            testUrl: ""
         }
     }
 });
