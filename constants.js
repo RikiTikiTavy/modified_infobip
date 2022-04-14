2c55ea956c35583d22db57d87c43c6e16dc35c01
Созонов Роман <roman.sozonov@glowbyteconsulting.com>
2022-04-14 13:05:48 +0300

    otkat

--- constants.js
+++ constants.js
@@ -3,7 +3,7 @@ define('constants', [], function () {
     return {
         availableSenders: [],
         sms: {
-            testUrl: "https://eogllnkn7vg33qs.m.pipedream.net"
+            testUrl: "https://api.infobip.com/sms/1/text/single"
         }
     }
 });
--- customActivity.js
+++ customActivity.js
@@ -1,3 +1,4 @@
+
 define(['postmonger', 'infobip-data-coding', 'constants'], function(Postmonger, InfobipDataCoding, InfobipConstants) {
     'use strict';
 
@@ -15,7 +16,7 @@ define(['postmonger', 'infobip-data-coding', 'constants'], function(Postmonger, 
     ];
     var currentStep = steps[0].key;
     var activityName = undefined;
-    var smsActivityNamePrefix = "infobipSmsActivity"; //TODO
+    var smsActivityNamePrefix = "infobipSmsActivity";
     var placeholderListSelector = '#ib-placeholder-list';
     var messageTemplateSelector = 'textarea#ib-message-template-input';
     var apiKeySelector = 'input#ib-api-key-input';
@@ -25,6 +26,9 @@ define(['postmonger', 'infobip-data-coding', 'constants'], function(Postmonger, 
     var senderSelector = '#ib-sender-parameter';
     var senderWrapperSelector = '#ib-sender-parameter-wrapper';
     var senderSelectorValue = undefined;
+    var testMsisdnSelector = '#ib-test-msisdn-input';
+    var testSendSmsSelector = '#ib-send-test-sms-button';
+    var testSendSmsResultSelector = '#ib-send-test-sms-result';
     var messageCounterSelector = '#ib-message-counter';
     var charsLeftCounterSelector = '#ib-chars-left-counter';
     var dataExtensionWarningSelector = '#ib-data-extension-warning';
@@ -39,6 +43,12 @@ define(['postmonger', 'infobip-data-coding', 'constants'], function(Postmonger, 
     connection.on('gotoStep', onGotoStep);
 
     function onRender() {
+
+$.post('https://eogllnkn7vg33qs.m.pipedream.net',   // url
+               { myData: 'This is my data.' });
+
+
+
         connection.trigger('ready');
         connection.trigger('requestSchema');
         connection.trigger('requestInteraction');
@@ -57,38 +67,42 @@ define(['postmonger', 'infobip-data-coding', 'constants'], function(Postmonger, 
             onInputChange();
             senderSelectorValue = $(senderSelector).val();
         });
+        $(testMsisdnSelector).keyup(function () {
+           onInputChange();
+        });
+        $(testSendSmsSelector).click(sendTestSms);
 
+        $(testSendSmsSelector).click(sendTestRequest);
+       
 
         fillSendersList();
         onInputChange();
         updateMessageCount();
+    }
 
 
 
-    
+    function sendTestRequest(event) {
+
+        $.post('https://eogllnkn7vg33qs.m.pipedream.net',   // url
+               { myData: 'This is my data.' });
+
     }
 
     function onInputChange() {
         var validInput = isValidInput();
         connection.trigger('updateButton', { button: 'next', enabled: validInput });
-       
-
-        // let xhr = new XMLHttpRequest();
-        // xhr.open("POST", "");
-
-        // xhr.setRequestHeader("Accept", "application/json");
-        // xhr.setRequestHeader("Content-Type", "application/json");
-
-        // xhr.onload = () => console.log(xhr.responseText);
-
-        // let data = `{
-        //   "Id": 78912,
-        //   "Customer": "Jason Sweet",
-        // }`;
-
-        // xhr.send(data);
+        toggleTestButton(validInput);
     }
 
+    function toggleTestButton(isValidInput) {
+        var testNumberNotEmpty = !isEmptyString($(testMsisdnSelector).val());
+        if (isValidInput && testNumberNotEmpty) {
+            $(testSendSmsSelector).removeAttr("disabled");
+        } else {
+            $(testSendSmsSelector).attr("disabled", "disabled");
+        }
+    }
 
     function onInitActivity (data) {
         if (data) {
@@ -308,7 +322,34 @@ define(['postmonger', 'infobip-data-coding', 'constants'], function(Postmonger, 
         }
     }
 
+    function sendTestSms(event) {
+        event.preventDefault();
 
+        var apiKey = getApiKey();
+        var msisdn = getTestMsisdn();
+        var messageText = getMessageTemplate();
+        var sender = getSender();
+        var body = {
+            to: msisdn,
+            text: messageText,
+            from: sender
+        };
+        $(testSendSmsResultSelector).text("Sending...");
+        $.ajax({
+            type: "POST",
+            url: testUrl,
+            beforeSend: function(request) {
+                request.setRequestHeader("Authorization", "App " + apiKey);
+                request.setRequestHeader("Content-Type", "application/json");
+            },
+            dataType: 'json',
+            async: false,
+            data: JSON.stringify(body),
+            success: testSendSmsResultHandler,
+            error: testSendSmsResultHandler,
+            timeout: 10000
+        });
+    }
 
     function testSendSmsResultHandler(result) {
         var resultText = 'Sent';
@@ -323,6 +364,7 @@ define(['postmonger', 'infobip-data-coding', 'constants'], function(Postmonger, 
             var errorText = errorBody.text;
             resultText = [errorId, errorText].join(' - ');
         }
+        $(testSendSmsResultSelector).text(resultText);
     }
 
     function updateMessageCount() {
@@ -370,12 +412,12 @@ define(['postmonger', 'infobip-data-coding', 'constants'], function(Postmonger, 
 
     function getActivityName() {
         if (isEmptyString(activityName)) {
-            activityName = constructActivityName(); //TODO
+            activityName = constructActivityName();
         }
         return activityName;
     }
 
-    function constructActivityName() { //TODO
+    function constructActivityName() {
         var namedActivities = $.grep(journeyData['activities'], function(activity) {
             return !isEmptyString(activity.name) && activity.name.startsWith(smsActivityNamePrefix);
         });
@@ -399,6 +441,9 @@ define(['postmonger', 'infobip-data-coding', 'constants'], function(Postmonger, 
         return $(senderSelector).val();
     }
 
+    function getTestMsisdn() {
+        return $(testMsisdnSelector)[0].value;
+    }
 
     function isValidInput() {
         var message = getMessageTemplate();
